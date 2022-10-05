a3cc5efc25715d466b97b19b000df8f57379b1f3
myy0309 <jp2019213114@qmul.ac.uk>
2022-10-05 16:51:49 +0800

    return reference

--- reference/main.cpp
+++ reference/main.cpp
@@ -8,21 +8,38 @@ void swap(int &i, int &j){
     j = temp;
 }
 
+int& test(){
+    static int a = 10; //a is not a local variable anymore
+    return a;
+}
+
+// illegal: return reference of local variable
+//int &test(){
+//    int a = 10;
+//    return a;
+//}
+
 int main(){
     int a = 1;
     // create a reference to a
-    const int & b = a;
-    
-    a = 2;
-    
+    int & b = a; // 自动转换为 int* const b = &a; 指针常量
+    b = 2; // 编译器发现b为引用，自动转换为*b = 2；
+
     cout << "a=" << a << endl;
     cout << "b=" << b << endl; // value of b changes with a
-    
+
     int i = 10;
     int j = 20;
     swap(i, j);
     cout << "i=" << i << endl;
     cout << "j=" << j << endl;
+    
+    int & ref = test();
+    cout << ref << endl;
+    cout << ref << endl;
+    test() = 100;
+    cout << ref << endl;
+    cout << ref << endl;
     
     
     return 0;
