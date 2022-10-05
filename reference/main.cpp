249a9baf06347480067fad27d3967b3c82a262cd
myy0309 <jp2019213114@qmul.ac.uk>
2022-10-05 17:26:36 +0800

    const reference

--- reference/main.cpp
+++ reference/main.cpp
@@ -1,46 +1,18 @@
 #include<iostream>
 using namespace std;
 
-// use reference as parameter of function
-void swap(int &i, int &j){
-    int temp = i;
-    i = j;
-    j = temp;
+// protect the input reference
+void printValue(const int & val){
+    // val = 100; illegal since val is const
+    cout << val << endl;
 }
-
-int& test(){
-    static int a = 10; //a is not a local variable anymore
-    return a;
-}
-
-// illegal: return reference of local variable
-//int &test(){
-//    int a = 10;
-//    return a;
-//}
 
 int main(){
-    int a = 1;
-    // create a reference to a
-    int & b = a; // 自动转换为 int* const b = &a; 指针常量
-    b = 2; // 编译器发现b为引用，自动转换为*b = 2；
+    
+    // int &ref = 10; illeagl
+    const int &ref = 10;
+    // 编译器会将代码修改为 int temp = 10;  const int & ref = temp;
+    // ref = 10; illeagl
 
-    cout << "a=" << a << endl;
-    cout << "b=" << b << endl; // value of b changes with a
-
-    int i = 10;
-    int j = 20;
-    swap(i, j);
-    cout << "i=" << i << endl;
-    cout << "j=" << j << endl;
-    
-    int & ref = test();
-    cout << ref << endl;
-    cout << ref << endl;
-    test() = 100;
-    cout << ref << endl;
-    cout << ref << endl;
-    
-    
     return 0;
 }
