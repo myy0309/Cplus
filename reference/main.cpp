b7efa2984de01ae5099d869de2ae8af4913335fa
myy0309 <jp2019213114@qmul.ac.uk>
2022-10-05 15:33:47 +0800

    reference as functoin parameter

--- reference/main.cpp
+++ reference/main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+// use reference as parameter of function
+void swap(int &i, int &j){
+    int temp = i;
+    i = j;
+    j = temp;
+}
+
 int main(){
     int a = 1;
     // create a reference to a
@@ -9,6 +16,14 @@ int main(){
     a = 2;
     
     cout << "a=" << a << endl;
-    cout << "b=" << b << endl;
+    cout << "b=" << b << endl; // value of b changes with a
+    
+    int i = 10;
+    int j = 20;
+    swap(i, j);
+    cout << "i=" << i << endl;
+    cout << "j=" << j << endl;
+    
+    
     return 0;
 }
