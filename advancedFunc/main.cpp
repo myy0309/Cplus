3ef6006311b7873902f46606ba9b0e61d55951e7
myy0309 <jp2019213114@qmul.ac.uk>
2022-10-06 16:23:36 +0800

    override function

--- advancedFunc/main.cpp
+++ advancedFunc/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace::std;
 
-// placeholder
-void func(int = 0){
-    cout << "this is a placeholder" << endl;
+// override function
+void func(int &a){
+    cout << "func(int &a)" << endl;
+}
+void func(const int &a){
+    cout << "func(const int &a)" << endl;
+}
+
+// override function with default parameter
+void func2(int a){
+    cout << "func2 without default parameter" << endl;
+}
+void func2(int a, int b = 0){
+    cout << "func2 with default parameter" << endl;
 }
 
 int main() {
-    func(10);
+    func2(0, 0);
+
+    int a = 0;
+    func(a); // func without const will be called
+    func(0); // func with const will be called
     return 0;
 }
